2f5af53c68a78c5ab1d7a256f48dd8d90527f190
Sallust <daniyom@gmail.com>
2015-12-14 13:54:38 -0500

    refactor: fix resize pizza slider function
    -console logs resize around 0.9ms

--- views/js/main.js
+++ views/js/main.js
@@ -421,43 +421,29 @@ var resizePizzas = function(size) {
 
   changeSliderLabel(size);
 
-   // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
-  function determineDx (elem, size) {
-    var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
-    var oldSize = oldWidth / windowWidth;
-
-    // TODO: change to 3 sizes? no more xl?
     // Changes the slider value to a percent width
-    function sizeSwitcher (size) {
+  function sizeSwitcher (size) {
       switch(size) {
         case "1":
-          return 0.25;
+          return 25;
         case "2":
-          return 0.3333;
+          return 33.3;
         case "3":
-          return 0.5;
+          return 50;
         default:
           console.log("bug in sizeSwitcher");
       }
     }
 
-    var newSize = sizeSwitcher(size);
-    var dx = (newSize - oldSize) * windowWidth;
-
-    return dx;
-  }
-
   // Iterates through pizza elements on the page and changes their widths
-  function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+  var allThePizzas = document.querySelectorAll(".randomPizzaContainer")
+  function changePizzaSizes(newWidth) {
+    for (var i = 0; i < allThePizzas.length; i++) {
+      allThePizzas[i].style.width = newWidth + "%";
     }
   }
 
-  changePizzaSizes(size);
+  changePizzaSizes(sizeSwitcher(size));
 
   // User Timing API is awesome
   window.performance.mark("mark_end_resize");
